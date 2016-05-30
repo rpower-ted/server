b5e0f70e04cec625c99760e5bfb28bb31bc972ed
Oleksandr Byelkin <sanja@mariadb.com>
2016-05-30 21:22:50 +0200

    MDEV-8931: (server part of) session state tracking
    Transaction tracker

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -689,10 +689,11 @@ typedef struct system_variables
 
   my_bool pseudo_slave_mode;
 
+  char *session_track_system_variables;
+  ulong session_track_transaction_info;
   my_bool session_track_schema;
   my_bool session_track_state_change;
 
-  char *session_track_system_variables;
 } SV;
 
 /**
@@ -4054,7 +4055,9 @@ private:
   LEX_STRING invoker_host;
 
 public:
+#ifndef EMBEDDED_LIBRARY
   Session_tracker session_tracker;
+#endif //EMBEDDED_LIBRARY
   /*
     Flag, mutex and condition for a thread to wait for a signal from another
     thread.
@@ -4288,6 +4291,8 @@ my_eof(THD *thd)
 {
   thd->set_row_count_func(-1);
   thd->get_stmt_da()->set_eof_status(thd);
+
+  TRANSACT_TRACKER(add_trx_state(thd, TX_RESULT_SET));
 }
 
 #define tmp_disable_binlog(A)                                              \
